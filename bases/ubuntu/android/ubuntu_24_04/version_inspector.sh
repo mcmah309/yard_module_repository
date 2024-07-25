972bf9b6a0dc924ade6bbd7963e37ca30ca0a6a1
mcmah309 <dillonmcmahon79@gmail.com>
2024-07-25 12:45:48 +0000

    feat: Add android on ubuntu 24.04

---
+++ bases/ubuntu/android/ubuntu_24_04/version_inspector.sh
@@ -0,0 +1,1 @@
+cat /etc/*release | grep VERSION= && java -version && gradle -v && kotlinc -version && cat $ANDROID_HOME/cmdline-tools/tools/source.properties && sdkmanager --list && apt list --installed | grep openssh-server
