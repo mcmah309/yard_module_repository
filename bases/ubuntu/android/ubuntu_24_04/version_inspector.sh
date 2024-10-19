a39c2e2f6f3b3c295532dc36317d51905b728311
mcmah309 <dillonmcmahon79@gmail.com>
2024-10-19 22:15:43 +0000

    feat: new ubuntu 24.04 android base

--- bases/ubuntu/android/ubuntu_24_04/version_inspector.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/*release | grep VERSION= && java -version && gradle -v && kotlinc -version && cat $ANDROID_HOME/cmdline-tools/tools/source.properties && sdkmanager --list && apt list --installed | grep openssh-server
